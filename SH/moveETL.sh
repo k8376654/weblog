5a7c33a77ad2c6840fa483ab1f9b54e60b1a8ef9
orozcohsu <orozcohsu@hotmail.com>
2018-07-13 23:06:51 +0800

    update

--- SH/moveETL.sh
+++ SH/moveETL.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-cd /hadoop/weblog/log;
+cd /tmp/weblog/log;
 FILES=$(find $dir -not -empty|sed -e '1,1d')
 
 for f in $FILES
 do
-  mv $f -f /hadoop/weblog/hadoop-log
+  mv $f -f /tmp/weblog/hadoop-log
 done
 
-cd /hadoop/weblog/hadoop-log
+cd /tmp/weblog/hadoop-log
 FILES=$(find $dir -not -empty|sed -e '1,1d')
+
 for i in $FILES
 do
 	mdate=$(echo $i | cut -c3-10)
 	sdate=$(echo $i | cut -c11-12)
-	hadoop fs -mkdir -p /user/hive/prestage/weblog/$mdate/$sdate
-	hadoop fs -copyFromLocal -f $i /user/hive/prestage/weblog/$mdate/$sdate
+	sudo -u hive hadoop fs -mkdir -p /user/hive/prestage/weblog/$mdate/$sdate
+        #For Ranger
+        sudo -u hive hadoop fs -chmod -R 777  /user/hive/prestage/weblog
+	sudo -u hive hadoop fs -copyFromLocal -f $i /user/hive/prestage/weblog/$mdate/$sdate
 done
 
-hadoop fs -mkdir -p /user/hive/prestage/weblog
-echo '' | hadoop fs -put -f - /user/hive/prestage/weblog/test.csv
